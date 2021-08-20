e2e7446f26c8947183a0199d8df4b87cb08f7fbe
barel <barelmas245@gmail.com>
2021-08-20 17:01:30 +0300

    initial cross validation code

--- d2d/orient_network.py
+++ d2d/orient_network.py
@@ -1,9 +1,14 @@
+import os
 import math
 import numpy
 import pandas
 import scipy
 import networkx as nx
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import plot_roc_curve, auc
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 from network.biogrid.read_biogrid import get_biogrid_network
 from breikreutz.experiments import get_gene_expressions_data
@@ -16,6 +21,9 @@ PROPAGATE_ITERATIONS = 100
 ORIENTATION_EPSILON = 0.01
 
 ANNOTATED_NETWORK_PATH = r'C:\git\BNet21-D2D\results\annotated_network.gpickle'
+FEATURE_COLS_PATH = r'C:\git\BNet21-D2D\results\features_cols.pickle'
+REVERSE_COLS_PATH = r'C:\git\BNet21-D2D\results\reverse_cols.pickle'
+CROSS_VALIDATION_ROC_PATH = r'C:\git\BNet21-D2D\results\cross_validation_roc.png'
 
 
 def read_data():
@@ -105,7 +113,7 @@ def generate_feature_columns(network, experiments_dict, true_annotations):
     return feature_columns, reverse_columns
 
 
-def score_network(feature_columns, reverse_columns, directed_interactions):
+def score_network(feature_columns, reverse_columns, directed_interactions, classifier):
     opposite_directed_interactions = list(map(lambda e: (e[1], e[0]), directed_interactions))
 
     true_in_feature = set(directed_interactions).intersection(feature_columns.index)
@@ -125,10 +133,10 @@ def score_network(feature_columns, reverse_columns, directed_interactions):
     reverse_labels = numpy.append(true_reverse_labels, false_reverse_labels)
     training_scores = numpy.append(feature_labels, reverse_labels)
 
-    classifier = LogisticRegression(solver="liblinear", penalty="l1", C=0.001).fit(training_columns, training_scores)
+    fitted_classifier = classifier.fit(training_columns, training_scores)
 
-    unclassified_feature_scores = classifier.predict_proba(feature_columns.drop(true_in_feature.union(false_in_feature)))[:, 1]
-    unclassified_reverse_scores = classifier.predict_proba(reverse_columns.drop(true_in_reverse.union(false_in_reverse)))[:, 1]
+    unclassified_feature_scores = fitted_classifier.predict_proba(feature_columns.drop(true_in_feature.union(false_in_feature)))[:, 1]
+    unclassified_reverse_scores = fitted_classifier.predict_proba(reverse_columns.drop(true_in_reverse.union(false_in_reverse)))[:, 1]
 
     feature_scores = numpy.concatenate((true_feature_labels, false_feature_labels, unclassified_feature_scores))
     reverse_scores = numpy.concatenate((true_reverse_labels, false_reverse_labels, unclassified_reverse_scores))
@@ -173,6 +181,71 @@ def orient_network(network, scores):
     return directed_network, annotated_edges
 
 
+def cross_validation(feature_columns, reverse_columns, directed_interactions, classifier):
+    opposite_directed_interactions = list(map(lambda e: (e[1], e[0]), directed_interactions))
+
+    true_in_feature = set(directed_interactions).intersection(feature_columns.index)
+    true_in_reverse = set(opposite_directed_interactions).intersection(reverse_columns.index)
+    false_in_feature = set(opposite_directed_interactions).intersection(feature_columns.index)
+    false_in_reverse = set(directed_interactions).intersection(reverse_columns.index)
+
+    training_columns = pandas.concat([feature_columns.loc[true_in_feature, :], feature_columns.loc[false_in_feature, :],
+                                      reverse_columns.loc[true_in_reverse, :],
+                                      reverse_columns.loc[false_in_reverse, :]])
+
+    true_feature_labels = numpy.ones(len(true_in_feature))
+    false_feature_labels = numpy.zeros(len(false_in_feature))
+    true_reverse_labels = numpy.ones(len(true_in_feature))
+    false_reverse_labels = numpy.zeros(len(false_in_feature))
+
+    feature_labels = numpy.append(true_feature_labels, false_feature_labels)
+    reverse_labels = numpy.append(true_reverse_labels, false_reverse_labels)
+    training_scores = numpy.append(feature_labels, reverse_labels)
+
+    standard_training_data = preprocessing.StandardScaler().fit_transform(training_columns)
+
+    cross_validator = StratifiedKFold(n_splits=10)
+
+    tprs = []
+    aucs = []
+    mean_fpr = numpy.linspace(0, 1, 100)
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    for i, (train, test) in enumerate(cross_validator.split(standard_training_data, training_scores)):
+        classifier.fit(standard_training_data[train], training_scores[train])
+        viz = plot_roc_curve(classifier, standard_training_data[test], training_scores[test],
+                             name='ROC fold {}'.format(i),
+                             alpha=0.3, lw=1, ax=ax)
+        interp_tpr = numpy.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+            label='Chance', alpha=.8)
+
+    mean_tpr = numpy.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = numpy.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    std_tpr = numpy.std(tprs, axis=0)
+    tprs_upper = numpy.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = numpy.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.' % (std_tpr))
+
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+           title="Receiver operating characteristic example")
+    ax.legend(loc="lower right")
+
+    plt.savefig(CROSS_VALIDATION_ROC_PATH)
+
+
 if __name__ == '__main__':
     network, true_annotations, experiments = read_data()
     true_annotations = list(map(lambda e: tuple(e), true_annotations))
@@ -180,8 +253,18 @@ if __name__ == '__main__':
     # TODO: check that all true directed interacions are inside the network
     # merged_network = pandas.concat([directed_interactions, network.drop(directed_interactions.index & network.index)])
 
-    feature_columns, reverse_columns = generate_feature_columns(network, experiments, true_annotations)
-    scores = score_network(feature_columns, reverse_columns, true_annotations)
-    annotated_network, annotated_edges = orient_network(network, scores)
+    if os.path.isfile(FEATURE_COLS_PATH) and os.path.isfile(REVERSE_COLS_PATH):
+        feature_columns = pandas.read_pickle(FEATURE_COLS_PATH)
+        reverse_columns = pandas.read_pickle(REVERSE_COLS_PATH)
+    else:
+        feature_columns, reverse_columns = generate_feature_columns(network, experiments, true_annotations)
+        feature_columns.to_pickle(FEATURE_COLS_PATH)
+        reverse_columns.to_pickle(REVERSE_COLS_PATH)
 
-    nx.write_gpickle(annotated_network, ANNOTATED_NETWORK_PATH)
+    classifier = LogisticRegression(solver="liblinear", penalty="l1", C=0.001)
+
+    cross_validation(feature_columns, reverse_columns, true_annotations, classifier)
+
+    # scores = score_network(feature_columns, reverse_columns, true_annotations, classifier)
+    # annotated_network, annotated_edges = orient_network(network, scores)
+    # nx.write_gpickle(annotated_network, ANNOTATED_NETWORK_PATH)
