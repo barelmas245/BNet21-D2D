ced307d34ec0dcf471ae0903a46c75e75019f46b
barel <barelmas245@gmail.com>
2021-08-22 22:47:56 +0300

    random

--- preprocessing/breikreutz/annotations.py
+++ preprocessing/breikreutz/annotations.py
@@ -1,8 +1,42 @@
+import networkx as nx
 import json
 import os
 
 from preprocessing.biogrid.read_biogrid import get_biogrid_network
+from preprocessing.breikreutz.experiments import get_gene_expressions_data
 from preprocessing.consts import RAW_BREITKREUTZ_ANNOTATIONS_DATA_PATH, GENERATED_BREITKREUTZ_ANNOTATIONS_PATH
+
+
+# def get_true_annotations(src_path=RAW_BREITKREUTZ_ANNOTATIONS_DATA_PATH,
+#                          dst_path=GENERATED_BREITKREUTZ_ANNOTATIONS_PATH,
+#                          filter_by_biogrid_net=True, force=False):
+#     if os.path.isfile(dst_path) and not force:
+#         with open(dst_path, 'r') as f:
+#             true_annotations_list = json.load(f)
+#             # Represent edges as tuples and not list
+#             return list(map(lambda e: tuple(e), true_annotations_list))
+#     else:
+#         if filter_by_biogrid_net:
+#             biogrid_net = get_biogrid_network()
+#             biogrid_genes = biogrid_net.nodes
+#
+#         true_annotations_list = []
+#         with open(src_path, 'r') as f:
+#             data = f.readlines()
+#         for entry in data:
+#             _, src_gene, _, _, dst_genes = entry.replace('\n', '').split('\t')
+#             for dst_gene in dst_genes.split(','):
+#                 if filter_by_biogrid_net and (src_gene not in biogrid_genes or dst_gene not in biogrid_genes):
+#                     continue
+#                 # Do not include both direction edges
+#                 if (dst_gene, src_gene) in true_annotations_list:
+#                     continue
+#                 true_annotations_list.append((src_gene, dst_gene))
+#
+#         with open(dst_path, 'w') as f:
+#             json.dump(true_annotations_list, f)
+#
+#         return true_annotations_list
 
 
 def get_true_annotations(src_path=RAW_BREITKREUTZ_ANNOTATIONS_DATA_PATH,
@@ -14,27 +48,45 @@ def get_true_annotations(src_path=RAW_BREITKREUTZ_ANNOTATIONS_DATA_PATH,
             # Represent edges as tuples and not list
             return list(map(lambda e: tuple(e), true_annotations_list))
     else:
-        if filter_by_biogrid_net:
-            biogrid_net = get_biogrid_network()
-            biogrid_genes = biogrid_net.nodes
+        biogrid_net = get_biogrid_network()
+        biogrid_genes = biogrid_net.nodes
+        experiments_dict = get_gene_expressions_data()
+        all_relevant_edges = set()
+        for source_gene in experiments_dict:
+            for target_gene in experiments_dict[source_gene]:
+                path = nx.shortest_path(biogrid_net, source=source_gene, target=target_gene)
+                edges_path = nx.utils.pairwise(path)
+                all_relevant_edges = all_relevant_edges.union(list(edges_path))
 
         true_annotations_list = []
         with open(src_path, 'r') as f:
             data = f.readlines()
-        for entry in data:
-            _, src_gene, _, _, dst_genes = entry.replace('\n', '').split('\t')
-            for dst_gene in dst_genes.split(','):
-                if filter_by_biogrid_net and (src_gene not in biogrid_genes or dst_gene not in biogrid_genes):
-                    continue
-                # Do not include both direction edges
-                if (dst_gene, src_gene) in true_annotations_list:
-                    continue
-                true_annotations_list.append((src_gene, dst_gene))
+        for entry in data[1:]:
+            gene1, sys_name1, gene2, sys_name2, assay, method, action, modification, pub_source, ref, note = entry.replace('\n', '').split('\t')
+            if filter_by_biogrid_net and (gene1 not in biogrid_genes or gene2 not in biogrid_genes):
+                continue
+
+            if method == 'high-throughput' and pub_source == 'BioGRID':
+                if action == 'Bait-Hit':
+                    edge = (gene1, gene2)
+                elif action == 'Hit-Bait':
+                    edge = (gene2, gene1)
+                else:
+                    raise Exception()
+
+            # Do not include both direction edges
+            if edge in true_annotations_list:
+                continue
+
+            reversed_edge = (edge[1], edge[0])
+            if edge in all_relevant_edges or reversed_edge in all_relevant_edges:
+                true_annotations_list.append(edge)
 
         with open(dst_path, 'w') as f:
             json.dump(true_annotations_list, f)
 
         return true_annotations_list
+
 
 
 if __name__ == '__main__':
--- preprocessing/breikreutz/experiments.py
+++ preprocessing/breikreutz/experiments.py
@@ -6,6 +6,49 @@ import os
 from preprocessing.biogrid.read_biogrid import get_biogrid_network
 from preprocessing.consts import RAW_BREITKREUTZ_EXPRESSIONS_DATA_PATH, GENERATED_BREITKREUTZ_EXPRESSIONS_PATH
 
+SOURCES = [
+    'SLN1',
+    'YCK1',
+    'YCK2',
+    'SHO1',
+    'MF(ALPHA)2',
+    'MID2',
+    'RAS2',
+    'GPR1',
+    'BCY1',
+    'STE50',
+    'MSB2',
+    'SIN3',
+    'RGA1',
+    'RGA2',
+    'ARR4'
+]
+
+TARGETS = [
+    'CDC42',
+    'HOG1',
+    'STE7',
+    'STE20',
+    'DIG2',
+    'DIG1',
+    'PBS2',
+    'FUS3',
+    'STE5',
+    'GPA1',
+    'FKS2',
+    'FUS1',
+    'STE12',
+    'SWI4'
+]
+
+TARGETS_BAREL = [
+    'FUS1',
+    'FAR1',
+    'GPD1',
+    'CTT1',
+    'GRE2'
+]
+
 
 def get_gene_expressions_data(src_path=RAW_BREITKREUTZ_EXPRESSIONS_DATA_PATH,
                               dst_path=GENERATED_BREITKREUTZ_EXPRESSIONS_PATH,
@@ -23,15 +66,28 @@ def get_gene_expressions_data(src_path=RAW_BREITKREUTZ_EXPRESSIONS_DATA_PATH,
         raw_data.drop(raw_data.columns[:6], axis=1, inplace=True)
 
         experiments_dict = dict()
-        for i in raw_data:
-            if filter_by_biogrid_net and i not in biogrid_genes:
+        all_sources = raw_data.columns
+        all_targets = raw_data.index
+        for src_gene in raw_data:
+            # if src_gene not in SOURCES:
+            #     continue
+
+            if (filter_by_biogrid_net and src_gene not in biogrid_genes) or src_gene in all_targets:
                 continue
-            targets_dict = raw_data[i].to_dict()
-            targets_dict = {key: value for key, value in targets_dict.items() if abs(value) >= np.log2(1.7)}
-            experiments_dict[i] = dict([(key, val) for key, val in targets_dict.items() if key in biogrid_genes]) if filter_by_biogrid_net else targets_dict
+            targets_dict = raw_data[src_gene].to_dict()
+            # Filter out targets which has no significant expression or has negative score
+            targets_dict = {key: value for key, value in targets_dict.items() if value <= - np.log2(1.7)}
+            # Filter out targets which are also in the sources
+            targets_dict = {key: value for key, value in targets_dict.items() if key not in all_sources}
+
+            # targets_dict = {key: value for key, value in targets_dict.items() if key in TARGETS or key in TARGETS_BAREL}
+
+            targets_dict = dict([(key, val) for key, val in targets_dict.items() if key in biogrid_genes]) if filter_by_biogrid_net else targets_dict
+            if targets_dict != {}:
+                experiments_dict[src_gene] = targets_dict
 
         with open(dst_path, 'w') as f:
-            json.dump(experiments_dict, f)
+            json.dump(experiments_dict, f, indent=2)
 
         return experiments_dict
 
