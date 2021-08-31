2fdf98a30d7c0b5d012cbbbd663a1530630f1c41
barel <barelmas245@gmail.com>
2021-08-31 12:05:20 +0300

    cross validations and roc

--- runs/yeast/consts.py
+++ runs/yeast/consts.py
@@ -1,5 +1,5 @@
 from runs.consts import RESULTS_DIR
 
 YEAST_RESULTS_DIR = RESULTS_DIR / 'yeast'
-CROSS_VALIDATION_ROC_PATH = YEAST_RESULTS_DIR / r'cross_validation_roc.png'
+CROSS_VALIDATION_ROC_PATH = YEAST_RESULTS_DIR / r'cross_validation_roc_{}.png'
 ANNOTATED_NETWORK_PATH = YEAST_RESULTS_DIR / r'annotated_network.gpickle'
--- runs/yeast/roc_cross_validation.py
+++ runs/yeast/roc_cross_validation.py
@@ -6,61 +6,68 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 
 from d2d.d2d import get_training_features_and_scores
+from d2d.propagation import PROPAGATE_METHODS
 from runs.yeast.data import read_data
 from runs.features import get_features
 from runs.yeast.consts import YEAST_RESULTS_DIR, CROSS_VALIDATION_ROC_PATH
 
 
-def cross_validation(feature_columns, reverse_columns, directed_interactions, classifier):
-    training_feature_columns, training_reverse_columns, training_feature_scores, training_reverse_scores = \
-        get_training_features_and_scores(feature_columns, reverse_columns, directed_interactions)
+PRIOR_WEIGHT_FUNCS = {
+    'unweighted': lambda x: 1,
+    'abs': abs,
+    'exp abs': lambda x: 2 ** abs(x)
+}
 
-    training_columns = pandas.concat([training_feature_columns, training_reverse_columns])
-    training_scores = np.append(training_feature_scores, training_reverse_scores)
 
-    cv = StratifiedKFold(n_splits=10)
+def roc_cross_validation(net_type, undirected):
+    network, true_annotations, experiments, edges_to_direct = read_data(net_type=net_type, undirected=undirected)
 
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
+    for propagation_method in PROPAGATE_METHODS:
+        fig, ax = plt.subplots()
+        for prior_weight_option in PRIOR_WEIGHT_FUNCS:
+            feature_cols, reverse_cols = get_features(network, experiments, edges_to_direct=edges_to_direct,
+                                                      output_directory=YEAST_RESULTS_DIR, method=propagation_method,
+                                                      prior_weight_func=PRIOR_WEIGHT_FUNCS[prior_weight_option],
+                                                      force=True, save=False)
+            logistic_regression_classifier = LogisticRegression(solver="liblinear", penalty="l1", C=0.001)
 
-    fig, ax = plt.subplots()
-    for i, (train, test) in enumerate(cv.split(training_columns, training_scores)):
-        classifier.fit(training_columns.iloc[train], training_scores[train])
-        viz = plot_roc_curve(classifier, training_columns.iloc[test], training_scores[test],
-                             name='ROC fold {}'.format(i),
-                             alpha=0.3, lw=1, ax=ax)
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
+            training_feature_columns, training_reverse_columns, training_feature_scores, training_reverse_scores = \
+                get_training_features_and_scores(feature_cols, reverse_cols, true_annotations)
 
-    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-            label='Chance', alpha=.8)
+            training_columns = pandas.concat([training_feature_columns, training_reverse_columns])
+            training_scores = np.append(training_feature_scores, training_reverse_scores)
 
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(mean_fpr, mean_tpr, color='b',
-            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-            lw=2, alpha=.8)
+            cv = StratifiedKFold(n_splits=10)
 
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. dev.')
+            tprs = []
+            aucs = []
+            mean_fpr = np.linspace(0, 1, 100)
 
-    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-           title="ROC curve")
-    ax.legend(loc="lower right")
-    plt.savefig(CROSS_VALIDATION_ROC_PATH)
+            for i, (train, test) in enumerate(cv.split(training_columns, training_scores)):
+                logistic_regression_classifier.fit(training_columns.iloc[train], training_scores[train])
+                viz = plot_roc_curve(logistic_regression_classifier, training_columns.iloc[test], training_scores[test])
+                interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+                interp_tpr[0] = 0.0
+                tprs.append(interp_tpr)
+                aucs.append(viz.roc_auc)
+
+            mean_tpr = np.mean(tprs, axis=0)
+            mean_tpr[-1] = 1.0
+            mean_auc = auc(mean_fpr, mean_tpr)
+            std_auc = np.std(aucs)
+            plt.figure(fig)
+            ax.plot(mean_fpr, mean_tpr,
+                    label=r'%s - Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (prior_weight_option, mean_auc, std_auc),
+                    lw=2, alpha=.8)
+        plt.figure(fig)
+        ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+                label='Chance', alpha=.8)
+        ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+               title=f"ROC curve - {propagation_method} diffusion")
+        ax.legend(loc="lower right")
+
+        plt.savefig(str(CROSS_VALIDATION_ROC_PATH).format(propagation_method))
 
 
 if __name__ == '__main__':
-    network, true_annotations, experiments = read_data()
-    feature_cols, reverse_cols = get_features(network, experiments,
-                                              output_directory=YEAST_RESULTS_DIR, force=False, save=True)
-    logistic_regression_classifier = LogisticRegression(solver="liblinear", penalty="l1", C=0.001)
-    cross_validation(feature_cols, reverse_cols, true_annotations, logistic_regression_classifier)
+    roc_cross_validation(net_type='biogrid', undirected=True)
